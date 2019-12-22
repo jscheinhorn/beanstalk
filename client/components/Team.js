e8595282dc236900192f6f545e1854d95bc6f615
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-12-21 23:00:19 -0500

    feature: add custom navbar, welcome page, create team component

---
+++ client/components/Team.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Team = () => (
+  <div className="block" style={{padding: '20px'}}>
+    <div className="card">Hoe bouy iy</div>
+  </div>
+)
+
+export default Team
