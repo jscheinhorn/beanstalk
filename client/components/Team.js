711c555a78f7139479fac68cee3e2a1427cd27cd
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-12-21 23:45:46 -0500

    feature: add team page

--- client/components/Team.js
+++ client/components/Team.js
@@ -2,7 +2,164 @@ import React from 'react'
 
 const Team = () => (
   <div className="block" style={{padding: '20px'}}>
-    <div className="card">Hoe bouy iy</div>
+    <div className="columns">
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Dr. Peyman Honarmandi</p>
+                <p className="subtitle is-6">Professor - Senior Design</p>
+              </div>
+            </div>
+            <div className="content">
+              E-mail: <a>phonarmandi@ ccny.cuny.edu</a>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Dr. Ali Sadegh</p>
+                <p className="subtitle is-6">Sponsor</p>
+              </div>
+            </div>
+            <div className="content">
+              E-mail: <a>sadegh@ccny.cuny.edu</a>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="columns">
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Jennifer Scheinhorn</p>
+                <p className="subtitle is-6">Project Lead</p>
+              </div>
+            </div>
+            <div className="content">
+              E-mail: <a>jscheinhorn@gmail.com</a>
+              <br />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Tariq Zaman</p>
+                <p className="subtitle is-6">Engineer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Adonis Diaz</p>
+                <p className="subtitle is-6">
+                  Engineer, Vice President ASME CCNY Chapter
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Navjeet Singh</p>
+                <p className="subtitle is-6">Engineer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/thumb/b/b2/City_College_of_New_York_seal.svg/1200px-City_College_of_New_York_seal.svg.png"
+                    alt="CCNY logo"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">Steven Bernal</p>
+                <p className="subtitle is-6">Engineer</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 )
 
